a31e7507563322b9c97ad2b90ed72f2d8f0d067a
Felipe Rojas <pradofeliperojas@gmail.com>
2025-03-16 20:40:25 -0500

    Update main to the specifications of the problem

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -4,8 +4,8 @@ import model.Controller;
 
 public class Main {
     public static void main(String[] args) {
-        int numEstudiantes = 20; // Número de estudiantes
-        int numSillasCorredor = 5; // Número de sillas en el corredor
+        int numEstudiantes = 5; // Número de estudiantes
+        int numSillasCorredor = 3; // Número de sillas en el corredor
 
         Controller controller = new Controller(numEstudiantes, numSillasCorredor);
         controller.iniciarSimulacion();
